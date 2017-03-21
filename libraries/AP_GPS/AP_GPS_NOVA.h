e5fbe9067b355fe94f9400310a3339411adfb4ee
ender-chen <imalexander@126.com>
2017-03-21 11:25:27 +0800

    Implement novatel driver, But not perfect.

--- libraries/AP_GPS/AP_GPS_NOVA.h
+++ libraries/AP_GPS/AP_GPS_NOVA.h
@@ -54,13 +54,11 @@ private:
     
     uint8_t _init_blob_index = 0;
     uint32_t _init_blob_time = 0;
-    const char* _initialisation_blob[6] = {
+    const char* _initialisation_blob[4] = {
         "\r\n\r\nunlogall\r\n", // cleanup enviroment
         "log bestposb ontime 0.2 0 nohold\r\n", // get bestpos
         "log bestvelb ontime 0.2 0 nohold\r\n", // get bestvel
-        "log psrdopb onchanged\r\n", // tersus
-        "log psrdopb ontime 0.2\r\n", // comnav
-        "log psrdopb\r\n" // poll message, as dop only changes when a sat is dropped/added to the visible list
+        "log psrdopb onchanged\r\n" // tersus
     };
    
     uint32_t crc_error_counter = 0;
